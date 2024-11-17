aad10c2a0f3a44f2372e995f987f007b6ca0e165
Giuseppe Criscione <18699708+giuscris@users.noreply.github.com>
2024-11-17 19:14:06 +0100

    Add favicon

--- site/templates/layouts/site.php
+++ site/templates/layouts/site.php
@@ -5,6 +5,7 @@
     <title><?= $page->title() ?> | <?= $site->title() ?></title>
     <?= $this->insert('_meta') ?>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🚧</text></svg>">
     <link rel="stylesheet" type="text/css" href="<?= $this->assets()->uri('css/style.min.css') ?>">
     <script src="<?= $this->assets()->uri('js/script.min.js') ?>"></script>
 </head>
