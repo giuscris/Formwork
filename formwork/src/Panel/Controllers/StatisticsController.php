0933b6e9e70bc92b8a4eeabf36255ed4711f09bd
Giuseppe Criscione <18699708+giuscris@users.noreply.github.com>
2025-02-09 12:48:37 +0100

    Add sources and devices to statistics view

--- formwork/src/Panel/Controllers/StatisticsController.php
+++ formwork/src/Panel/Controllers/StatisticsController.php
@@ -18,12 +18,18 @@ final class StatisticsController extends AbstractController
         }
 
         $pageViews = $statistics->getPageViews();
+        $sources = $statistics->getSources();
+        $devices = $statistics->getDevices();
 
         return new Response($this->view('statistics.index', [
             'title'             => $this->translate('panel.statistics.statistics'),
             'statistics'        => Json::encode($statistics->getChartData(30)),
             'pageViews'         => array_slice($pageViews, 0, 15, preserve_keys: true),
             'totalViews'        => array_sum($pageViews),
+            'sources'           => $sources,
+            'totalSources'      => array_sum($sources),
+            'devices'           => $devices,
+            'totalDevices'      => array_sum($devices),
             'monthVisits'       => array_sum($statistics->getVisits(30)),
             'weekVisits'        => array_sum($statistics->getVisits(7)),
             'monthUniqueVisits' => array_sum($statistics->getUniqueVisits(30)),
